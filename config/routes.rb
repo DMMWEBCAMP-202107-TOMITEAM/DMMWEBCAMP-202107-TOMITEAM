f120277571eb6e556d5b82edb26d3fc598e94e4b
tana-16 <tanahiro1697@gmail.com>
2021-08-15 09:42:36 +0000

    ルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  # root to: 'homes#top'
+  # get 'home/about' => 'homes#about'
   devise_for :clients
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin
+
+  namespace :admin do
+    resources :items
+    resources :genres,only: [:index,:create,:edit,:update]
+    resources :clients,only: [:index,:show,:edit,:update]
+    resources :orders,only: [:index,:show,:update]
+  end
+
+  resources :clients,only: [:show,:edit,:update] do
+  get "clients//:id" => "clients#check"
+  get "clients/:id" => "clients#out"
+  end
+
+  resources :items,only: [:index,:show]
+
+  resources :cart_items,only: [:index,:update,:create,:destroy] do
+  delete "cart_items" => "cart_items#destroy_all"
+  end
+
+  resources :orders,only: [:new,:index,:create,:show] do
+  get "orders" => "orders#check"
+  get "orders" => "orders#thanks"
+  end
+
+  resources :adresses,only: [:index,:update,:create,:destroy,:edit]
+
+  # get '/search', to: 'searches#search'
+
 end
