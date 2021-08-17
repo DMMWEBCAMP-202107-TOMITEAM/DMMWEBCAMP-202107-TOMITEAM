d434968756649a6e62551196262a8de8278da736
EC2 Default User <ec2-user@ip-172-31-36-171.ec2.internal>
2021-08-17 17:39:17 +0000

    配送先一覧ページ作成中

--- app/controllers/client/adresses_controller.rb
+++ app/controllers/client/adresses_controller.rb
@@ -1,2 +1,20 @@
 class Client::AdressesController < ApplicationController
+  
+  def index
+    @adress = Adress.new
+  end
+  
+  def create
+  	@adress = Adress.new(adress_params)
+  	@adress.client_id = current_client.id
+  	@adress.save
+  	redirect_to adresses_path
+  end
+  
+  private
+  
+  def adress_params
+  	params.require(:adress).permit(:name, :client_id, :adress, :postal_code)
+  end
+  
 end
--- app/models/adress.rb
+++ app/models/adress.rb
@@ -1,9 +1,4 @@
 class Adress < ApplicationRecord
 
-  eunm production_status:{
-    "販売中": 0,
-    "売り切れ": 1
-  }
-
   belongs_to :client
 end
