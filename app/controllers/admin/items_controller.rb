7ad38fc7b3940cc2d786076811d5aa1544235e97
yuki010126 <kinyounokyuzitu@gmail.com>
2021-08-18 04:22:55 +0000

    admin_0818

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,30 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all.page(params[:page]).per(10)
   end
+
+
+  def new
+    @item = Item.new
+  end
+
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
+  def create
+    @item = Item.new(item_params)
+    if @product.save
+      flash[:notice] = "新商品登録完了"
+      redirect_to admin_item_path(@product)
+    else
+      render :new
+    end
+  end
+
+
 end
+
+
